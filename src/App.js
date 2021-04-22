eba649043ce76eefeecc92f4586c3ec1b3154bea
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-22 20:50:17 +0600

    use for a simple case

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ReducerCount from './components/ReducerCount/ReducerCount';
 
 function App() {
   return (
     <div className="App">
-     
+      <ReducerCount />
     </div>
   );
 }

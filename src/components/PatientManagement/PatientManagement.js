4009a4b4a32795490ee3de7cc3dd1ab4d2481cc9
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-22 21:29:47 +0600

    UseReducer with action and payload

--- src/components/PatientManagement/PatientManagement.js
+++ src/components/PatientManagement/PatientManagement.js
@@ -1,14 +1,29 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import { patientReducer, patientState } from '../../reducers/patientReducer';
 
 const PatientManagement = () => {
+    const nameRef = useRef();
     const [state, dispatch] = useReducer(patientReducer, patientState);
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        dispatch({
+            type: 'ADD_PATIENT', 
+            name: nameRef.current.value, 
+            id: state.patients.length + 1
+        })
+        nameRef.current.value = '';
+        console.log(nameRef.current.value);
+    }
     return (
         <div>
-            <h1>Patient Management: {state.patients.count}</h1>
-            <form action="">
-                <input value=""/>
+            <h1>Patient Management: {state.patients.length}</h1>
+            <form onSubmit={handleSubmit}>
+                <input ref={nameRef}></input>
             </form>
+            {
+                state.patients.map(pt => <li key={pt.id} onClick={() => dispatch({type: 'REMOVE_PATIENT', id: pt.id})}>{pt.name}</li>)
+            }
         </div>
     );
 };
